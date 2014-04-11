10558d522e713952fbd9ddb15aef367b892d4ce1
sj-CUNY <sjain@york.cuny.edu>
2014-04-11 13:54:53 -0400

    Deleted redundant files

--- ns-allinone-3.19/ns-3.19/build/.conf_check_4a01e433a529e3f36177a00d38e73ee3/test.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "Brite.h"
-
-int main()
-{
-  return 0;
-}
--- ns-allinone-3.19/ns-3.19/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/subdir/ns3.19-subdir-debug', 'build/scratch/ns3.19-igraph-debug', 'build/scratch/ns3.19-wifi-ap-debug', 'build/scratch/ns3.19-brite-generic-example_orig-debug', 'build/scratch/ns3.19-wifi-ap-orig-debug', 'build/scratch/ns3.19-scratch-simulator-debug', 'build/scratch/ns3.19-brite-generic-example-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
