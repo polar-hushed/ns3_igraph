568c728d5b9375b53bff71a7ac42ae6792eed185
sj-CUNY <sjain@york.cuny.edu>
2014-06-02 15:55:19 -0400

    SAP for nsclick

---
+++ ns-allinone-3.19/ns-3.19/scratch/nsclick-MF_routing.cc
@@ -0,0 +1,136 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Lalith Suresh <suresh.lalith@gmail.com>
+ */
+
+// Network topology
+//
+//
+//              172.16.1.0/24
+//        (1.1)  (1.2)  (2.1)  (2.2)
+//
+//         eth0   eth0  eth1    eth0
+//       n0 ========= n1 ========= n2
+//            LAN 1       LAN 2
+//
+// - UDP flows from n0 to n2 via n1.
+// - All nodes are Click based.
+//
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/ipv4-click-routing.h"
+#include "ns3/ipv4-l3-click-protocol.h"
+#include "ns3/click-internet-stack-helper.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("NsclickRouting");
+
+int
+main (int argc, char *argv[])
+{
+#ifdef NS3_CLICK
+
+//
+// Explicitly create the nodes required by the topology (shown above).
+//
+  std::map<std::string, std::string> defines;
+  NS_LOG_INFO ("Create nodes.");
+  NodeContainer n;
+  n.Create (5);
+
+//
+// Install Click on the nodes
+//
+  ClickInternetStackHelper clickinternet;
+  clickinternet.SetClickFile (n.Get (0), "src/click/examples/MF_BasicRouter.click");
+  clickinternet.SetClickFile (n.Get (1), "src/click/examples/MF_BasicRouter.click");
+  clickinternet.SetClickFile (n.Get (2), "src/click/examples/MF_BasicRouter.click");
+  clickinternet.SetClickFile (n.Get (3), "src/click/examples/MF_BasicRouter.click");
+  for (int i = 0; i < 4; ++i)
+  {
+  	clickinternet.SetRoutingTableElement (n.Get (i), "u/rt");
+	defines["MY_GUID"] = i;
+  	clickinternet.SetDefines(n.Get(i), defines);
+  }
+  clickinternet.Install (n);
+
+  NS_LOG_INFO ("Create channels.");
+//
+// Explicitly create the channels required by the topology (shown above).
+//
+  CsmaHelper csma;
+  csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (5000000)));
+  csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
+  csma.SetDeviceAttribute ("Mtu", UintegerValue (1400));
+  NetDeviceContainer d[5];
+  for (int i = 0; i < 4; ++i)
+	  d[i] = csma.Install (NodeContainer (n.Get (i), n.Get (i+1)));
+
+  Ipv4AddressHelper ipv4;
+//
+// We've got the "hardware" in place.  Now we need to add IP addresses.
+//
+  NS_LOG_INFO ("Assign IP Addresses.");
+  ipv4.SetBase ("172.16.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer ip[5];
+  for (int i = 0; i < 4; ++i)
+	ip[i] = ipv4.Assign (d[i]);
+
+  NS_LOG_INFO ("Create Applications.");
+//
+// Create one udpServer applications on node one.
+//
+  uint16_t port = 4000;
+  UdpServerHelper server (port);
+  ApplicationContainer apps = server.Install (n.Get (0));
+  apps.Start (Seconds (1.0));
+  apps.Stop (Seconds (10.0));
+
+//
+// Create one UdpClient application to send UDP datagrams from node zero to
+// node one.
+//
+  uint32_t MaxPacketSize = 1024;
+  Time interPacketInterval = Seconds (0.05);
+  uint32_t maxPacketCount = 320;
+  UdpClientHelper client (ip[0].GetAddress (1), port);
+  client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+  client.SetAttribute ("Interval", TimeValue (interPacketInterval));
+  client.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
+  apps = client.Install (NodeContainer (n.Get (4)));
+  apps.Start (Seconds (2.0));
+  apps.Stop (Seconds (10.0));
+
+  csma.EnablePcap ("nsclick-routing", d[1], false);
+  csma.EnablePcap ("nsclick-routing", d[2], false);
+
+//
+// Now, do the actual simulation.
+//
+  NS_LOG_INFO ("Run Simulation.");
+  Simulator::Stop (Seconds (20.0));
+  Simulator::Run ();
+  Simulator::Destroy ();
+  NS_LOG_INFO ("Done.");
+#else
+  NS_FATAL_ERROR ("Can't use ns-3-click without NSCLICK compiled in");
+#endif
+}
